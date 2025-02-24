76169cf3019a403d5ad0444f7a0f024bfdced931
Frank Denis <github@pureftpd.org>
2025-02-25 00:38:22 +0100

    Import libaegis files, not as a projet

---
+++ common/libaegis/src/aegis128l/aegis128l_armcrypto.h
@@ -0,0 +1,9 @@
+#ifndef aegis128l_armcrypto_H
+#define aegis128l_armcrypto_H
+
+#include "../common/common.h"
+#include "implementations.h"
+
+extern struct aegis128l_implementation aegis128l_armcrypto_implementation;
+
+#endif
